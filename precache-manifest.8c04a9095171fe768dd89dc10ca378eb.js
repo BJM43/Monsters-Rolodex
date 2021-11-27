654cf1536f381ab58203b28c624068233407c49e
bjm43 <bjm-43@hotmail.com>
2021-11-26 21:52:16 -0400

    Updates

---
+++ precache-manifest.8c04a9095171fe768dd89dc10ca378eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8e4d411449868f1555b70604c2ddda",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "c98b21df28aa95712a65",
+    "url": "/Monsters-Rolodex/static/css/main.aadc03ab.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/Monsters-Rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Monsters-Rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e36c4017ec0df4e6e55",
+    "url": "/Monsters-Rolodex/static/js/3.7b815fc8.chunk.js"
+  },
+  {
+    "revision": "c98b21df28aa95712a65",
+    "url": "/Monsters-Rolodex/static/js/main.eb35dd0c.chunk.js"
+  },
+  {
+    "revision": "c717e52cc0e2fe56ff87",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.db69a1b5.js"
+  }
+]);
